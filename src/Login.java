cbb236e8f8adb22e8236b40693780da37a16aa7b
Luxyz <elkin.diaz@epn.edu.ec>
2024-12-10 19:34:36 -0500

    commit final

--- src/Login.java
+++ src/Login.java
@@ -1,7 +1,27 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login {
     private JTextField textField1;
     private JPasswordField passwordField1;
-    private JPanel Calculadora;
+    public JPanel Login;
+    private JButton logInButton;
+    private JLabel validacion;
+
+    public Login() {
+
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (textField1.getText().equals("APELLIDO") && passwordField1.getText().equals("APELLIDO123")) {
+                    validacion.setText("Acceso exitoso");
+                }else {
+                    validacion.setText("Credenciales invalidas");
+                }
+            }
+        });
+
+
+    }
 }
