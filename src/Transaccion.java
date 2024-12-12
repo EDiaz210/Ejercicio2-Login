06fbb60a4f71435385dd741b02e6a90d8970854f
EDiaz210 <elkin.diaz@epn.du.ec>
2024-12-12 07:49:44 -0500

    commit2

---
+++ src/Transaccion.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Transaccion {
+    private JButton transaccionesButton;
+    private JButton depositarButton;
+    private JButton salirButton;
+    private JButton retirarButton;
+
+    public Transaccion() {
+        depositarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
